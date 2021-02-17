f390e304e3fd0d3be1af275e39e3ecac1801f129
jgaspar <joaopgaspar98@gmail.com>
2021-02-17 16:21:43 +0000

    added some more css

--- routes/movies.js
+++ routes/movies.js
@@ -57,7 +57,6 @@ router.get('/results', (req, res) => {
     let searchFor = req.query.general;
     // imdb Api documentation to search for a  non specific film, returning a list of films
     imdb.search({ name: searchFor }, { apiKey: process.env.API_KEY }).then((response) => {
-        console.log(response.results);
         res.render('results', { results: response.results });
     }).catch(err => {
         console.log(err);
